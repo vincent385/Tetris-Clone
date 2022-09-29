67e476889849749e50110c677c5d65367614edf7
vincent385 <vincentbell103@outlook.com>
2022-09-29 01:58:05 +0100

    created component files with scaffolding

---
+++ src/components/Cell.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Cell ({ type }) {
+    return (
+        <div>cell</div>
+    );
+}
+
+export default Cell;
---
+++ src/components/Display.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Display ({ gameOver, text }) {
+    return (
+        <div>{text}</div>
+    );
+}
+
+export default Display;
