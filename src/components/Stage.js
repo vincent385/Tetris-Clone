4cfa83bd0ce8fd51342aeefa096ccf547ffa364a
vincent385 <vincentbell103@outlook.com>
2022-09-30 06:49:44 +0100

    made functions and constants for handling stages

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -3,7 +3,9 @@ import Cell from './Cell'
 
 function Stage ({ stage }) {
     return (
-        <div><Cell /></div>
+        <div>
+            { stage.map(row => row.map((cell, x) => <Cell key = { x } type = { cell[0] } />)) }
+        </div>
     );
 }
 
